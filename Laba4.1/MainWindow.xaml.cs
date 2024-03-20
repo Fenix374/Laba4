a00a6c2ad45719a918621aeb08f946a60d6466ab
Fenix374 <phoenilysenko@gmail.com>
2024-03-20 15:32:58 +0300

    Laba4.1

---
+++ Laba4.1/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using Laba4._1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace Laba5
+{
+    public partial class MainWindow : Window
+    {
+        private ASSASINS_CREEDEntities db = new ASSASINS_CREEDEntities();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            FillPlatformComboBox();
+        }
+
+        private void FillPlatformComboBox()
+        {
+
+                List<string> platforms = db.GAMES.Select(g => g.PLATFORMS).Distinct().ToList();
+
+                PlatformComboBox.Items.Add("Filter by Platform...");
+
+                foreach (string platform in platforms)
+                {
+                    PlatformComboBox.Items.Add(platform);
+                }
+
+                PlatformComboBox.SelectedIndex = 0;
+            
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            string searchText = SearchTextBox.Text.Trim();
+
+            if (!string.IsNullOrEmpty(searchText))
+            {
+                var games = db.GAMES.Where(g => g.TITLE.Contains(searchText) || g.PLATFORMS.Contains(searchText)).ToList();
+                GamesDataGrid.ItemsSource = games;
+            }
+            
+        }
+
+        private void FilterButton_Click(object sender, RoutedEventArgs e)
+        {
+            string platformFilter = PlatformComboBox.SelectedItem as string;
+
+            if (!string.IsNullOrEmpty(platformFilter))
+            {
+                var games = db.GAMES.Where(g => g.PLATFORMS.Equals(platformFilter)).ToList();
+                GamesDataGrid.ItemsSource = games;
+            }
+            
+        }
+    }
+}
---
+++ Laba4.1/Window1.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows;
+
+namespace Laba5
+{
+    public partial class Window1 : Window
+    {
+        private DataSet dataSet = new DataSet();
+
+        public Window1()
+        {
+            InitializeComponent();
+            FillPlatformComboBox();
+        }
+
+        private void FillPlatformComboBox()
+        {
+                using (var connection = new SqlConnection())
+                {
+                    connection.Open();
+
+                    using (var command = new SqlCommand("FAM", connection))
+                    {
+                        using (var adapter = new SqlDataAdapter(command))
+                        {
+                            adapter.Fill(dataSet, "Platforms");
+
+                            foreach (DataRow row in dataSet.Tables["Platforms"].Rows)
+                            {
+                                PlatformComboBox.Items.Add(row["PLATFORMS"].ToString());
+                            }
+
+                            PlatformComboBox.Items.Insert(0, "Filter by Platform...");
+                            PlatformComboBox.SelectedIndex = 0;
+                        }
+                    }
+                }
+
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            string searchText = SearchTextBox.Text.Trim();
+            if (!string.IsNullOrEmpty(searchText))
+            {
+                
+                    using (var connection = new SqlConnection())
+                    {
+                        connection.Open();
+                        using (var command = new SqlCommand("SELECT * FROM GAMES WHERE TITLE LIKE @SearchText OR PLATFORMS LIKE @SearchText", connection))
+                        {
+                            command.Parameters.AddWithValue("@SearchText", "%" + searchText + "%");
+                            using (var adapter = new SqlDataAdapter(command))
+                            {
+                                dataSet.Tables["Games"].Clear();
+                                adapter.Fill(dataSet, "Games");
+                            }
+                        }
+                    }
+                
+            }
+
+        }
+
+        private void FilterButton_Click(object sender, RoutedEventArgs e)
+        {
+            string platformFilter = PlatformComboBox.SelectedItem as string;
+            if (!string.IsNullOrEmpty(platformFilter) && platformFilter != "Filter by Platform...")
+            {
+                    using (var connection = new SqlConnection())
+                    {
+                        connection.Open();
+                        using (var command = new SqlCommand("SELECT * FROM GAMES WHERE PLATFORMS = @Platform", connection))
+                        {
+                            command.Parameters.AddWithValue("@Platform", platformFilter);
+                            using (var adapter = new SqlDataAdapter(command))
+                            {
+                                dataSet.Tables["Games"].Clear();
+                                adapter.Fill(dataSet, "Games");
+                            }
+                        }
+                    }
+                }
+        }
+    }
+}
